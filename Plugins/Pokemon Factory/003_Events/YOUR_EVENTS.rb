03eca15e5bf0a901effe555f6f8ca4202bc6d698
Cautioncrazy <cautioncrazy23@gmail.com>
2026-04-14 18:33:55 -0400

    edited boss pokemon hash and hp png

--- Plugins/Pokemon Factory/003_Events/YOUR_EVENTS.rb
+++ Plugins/Pokemon Factory/003_Events/YOUR_EVENTS.rb
@@ -83,7 +83,7 @@ PokemonFactory.register(:wartortle_gacha, {
     species: :CASTFORM,
     level: 22,
     nickname: "Aether",
-    total_tiers: 4,
+    hp_level: 4,
     item: :LIFEORB,
     poke_ball: :CHERISHBALL,
     shiny: true,
